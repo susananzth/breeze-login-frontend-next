bed4c352a7829de929d814c85e7cc0cd26ab335c
susananzth <susananzth.dev@gmail.com>
2023-05-04 17:24:14 -0500

    Agregando funcionalidad de multilenguaje

---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,48 @@
+// LanguageSelector.js
+import translations from '@/lang/translation.json'
+import axios from '@/lib/axios'
+import Cookies from 'js-cookie'
+import { useEffect, useState } from 'react'
+import Button from '@/components/Button'
+
+const LanguageSelector = () => {
+
+    const [language, setLanguage] = useState('en')
+
+    useEffect(() => {
+        async function fetchData() {
+            const { data } = await axios.get('/api/language')
+
+            setLanguage(data.language)
+        }
+        fetchData()
+    }, [])
+
+    const t = (key, language) => {
+        return translations[language][key]
+    }
+    const handleLanguageChange = newLanguage => {
+        setLanguage(newLanguage)
+        // Send a request to the backend to update the language
+        async () => {
+            axios.post('/api/language', {
+                language: newLanguage,
+            })
+        }
+    }
+    return (
+        <div>
+            <h1>{t('welcome', language)}</h1>
+            <Button
+                onClick={() => handleLanguageChange('en')}>
+                English
+            </Button>
+            <Button
+                onClick={() => handleLanguageChange('es')}>
+                Español
+            </Button>
+        </div>
+    )
+}
+
+export default LanguageSelector
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useAuth } from '@/hooks/auth'
 import AppLayout from '@/components/Layouts/AppLayout'
+import LanguageSelector from '@/components/LanguageSelector';
 import Head from 'next/head'
 
 const Dashboard = () => {
@@ -42,6 +43,9 @@ const Dashboard = () => {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
+                            <div>
+                                <LanguageSelector />
+                            </div>
                             You're logged in!
                         </div>
                     </div>
